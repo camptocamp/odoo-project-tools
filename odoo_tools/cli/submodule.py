ba5546fbbb466be1266ff18301b1c8f5f2c1aaf8
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-12-22 16:39:08 +0100

    Add submodule.list (#132)
    List submodules paths which can be directly used to directly copy-paste the
    addons paths in the Dockerfile. The order depends of the order in the
    .gitmodules file.
    ---------
    
    This commit has been extracted from https://github.com/camptocamp/odoo-template

--- odoo_tools/cli/submodule.py
+++ odoo_tools/cli/submodule.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 from __future__ import print_function
 
+from itertools import chain
 
 from invoke import task, exceptions
 from .common import (
@@ -23,7 +24,7 @@ def init(ctx):
 
     """
     gitmodules = build_path('.gitmodules')
-    res = ctx.run("git config -f %s --get-regexp '^submodule\..*\.path$'" %
+    res = ctx.run(r"git config -f %s --get-regexp '^submodule\..*\.path$'" %
                   gitmodules, hide=True)
     odoo_version = cookiecutter_context()['odoo_version']
     with cd(root_path()):
@@ -37,3 +38,41 @@ def init(ctx):
                         (odoo_version, url.strip(), path.strip()))
             except exceptions.Failure:
                 pass
+
+    print("Submodules added")
+    print()
+    print("You can now update odoo/Dockerfile with this addons-path:")
+    print()
+    list(ctx)
+
+
+@task(help={
+    'dockerfile': 'With --no-dockerfile, the raw paths are listed instead '
+                  'of the Dockerfile format'
+})
+def list(ctx, dockerfile=True):
+    """ list git submodules paths
+
+    It can be used to directly copy-paste the addons paths in the Dockerfile.
+    The order depends of the order in the .gitmodules file.
+
+    """
+    gitmodules = build_path('.gitmodules')
+    res = ctx.run(
+        "git config --file %s "
+        "--get-regexp path | awk '{ print $2 }' " % gitmodules,
+        hide=True,
+    )
+    content = res.stdout
+    if dockerfile:
+        blacklist = {'odoo/src'}
+        lines = (line for line in content.splitlines()
+                 if line not in blacklist)
+        lines = chain(lines, ['odoo/src/addons', 'odoo/local-src'])
+        lines = ("/opt/%s" % line for line in lines)
+        template = (
+            "ENV ADDONS_PATH=\"%s\" \\\n"
+        )
+        print(template % (', \\\n'.join(lines)))
+    else:
+        print(content)
