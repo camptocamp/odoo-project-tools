d2d9ef6942e21d1e66f211b2b2ddb7b2e7ff3428
Tonow-c2c <thomas.nowicki@camptocamp.com>
2019-11-28 09:31:02 +0100

    Fix the right date for Copyright
    ---------
    
    This commit has been extracted from https://github.com/camptocamp/odoo-template

--- odoo_tools/cli/submodule.py
+++ odoo_tools/cli/submodule.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Camptocamp SA
+# Copyright {% now 'utc', '%Y' %} Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 from __future__ import print_function
 
