d76c57fc022dfe6e28cf89236fe0923f2f5595e8
Simone Orsi <simahawk@gmail.com>
2023-06-08 09:17:09 +0200

    Add utils.config

---
+++ odoo_tools/config.py
@@ -0,0 +1,26 @@
+# Copyright 2023 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+# TODO: use a config file or an env file
+
+from pathlib import PosixPath
+
+confs = {
+    "c2c_git_remote": "camptocamp",
+    # Path where odoo source code is available
+    # FIXME: new project structure won't have it
+    "odoo_src_rel_path": "odoo/src",
+    # Path where external sources are checked out
+    # being submodules or not.
+    # FIXME: will be dev-src
+    "ext_src_rel_path": "odoo/external-src",
+    "local_src_rel_path": "odoo/local-src",
+    "pending_merge_rel_path": "pending-merges.d",
+}
+
+
+def get_conf_key(key):
+    v = confs.get(key)
+    if key.endswith("_path"):
+        v = PosixPath(v)
+    return v
