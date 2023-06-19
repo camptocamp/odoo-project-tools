ffdd343106f97a49df455af22ecae24414a862bf
Simone Orsi <simahawk@gmail.com>
2023-06-19 12:55:45 +0200

    utils.pkg: improve class

--- tests/test_utils_pkg.py
+++ tests/test_utils_pkg.py
@@ -13,15 +13,34 @@ def test_pkg_class():
     mock_pypi_version_cache(f"odoo-addon-{addon_name}", "1.9.0")
     with fake_project_root():
         pkg = pkg_utils.Package(addon_name)
-    assert pkg.odoo
-    assert pkg.name == addon_name
-    assert pkg.pypi_name == f"odoo-addon-{addon_name}"
-    assert pkg.latest_version == "1.9.0"
-    assert pkg.pinned_version is None
-    old_req = f"{pkg.pypi_name} @ git+https://github.com/OCA/repo@refs/pull/3/head#subdirectory=setup/{pkg.name}"
+        assert pkg.odoo
+        assert pkg.name == addon_name
+        assert pkg.pypi_name == f"odoo-addon-{addon_name}"
+        assert pkg.latest_version == "1.9.0"
+        assert pkg.pinned_version is None
+
+
+def test_pkg_class_has_pending_merge():
+    addon_name = "edi_oca"
+    mock_pypi_version_cache(f"odoo-addon-{addon_name}", "1.9.0")
     with fake_project_root():
+        pkg = pkg_utils.Package(addon_name)
+        old_req = f"{pkg.pypi_name} @ git+https://github.com/OCA/repo@refs/pull/3/head#subdirectory=setup/{pkg.name}"
         with open("./requirements.txt", "w") as fd:
             fd.write(old_req)
         req_path = os.getcwd() + "/requirements.txt"
         pkg = pkg_utils.Package(addon_name, req_filepath=req_path)
-    assert pkg.has_pending_merge()
+        assert pkg.has_pending_merge()
+
+
+def test_pkg_class_is_editable():
+    addon_name = "edi_oca"
+    mock_pypi_version_cache(f"odoo-addon-{addon_name}", "1.9.0")
+    with fake_project_root():
+        pkg = pkg_utils.Package(addon_name)
+        old_req = f"-e path/to/module/setup/{addon_name}"
+        with open("./requirements.txt", "w") as fd:
+            fd.write(old_req)
+        req_path = os.getcwd() + "/requirements.txt"
+        pkg = pkg_utils.Package(addon_name, req_filepath=req_path)
+        assert pkg.is_editable()
