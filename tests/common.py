3b33a2ca3544ea5aa82dc6ff713c4e22aa93758c
Simone Orsi <simahawk@gmail.com>
2023-06-21 13:57:30 +0200

    Improve tests.common

--- tests/common.py
+++ tests/common.py
@@ -7,6 +7,7 @@ from pathlib import PosixPath
 from click.testing import CliRunner
 
 from odoo_tools.utils.path import get_root_marker
+from odoo_tools.utils.yaml import update_yml_file
 
 FIXTURES_PATH = PosixPath(__file__).parent / "fixtures"
 
@@ -27,34 +28,41 @@ def mock_pypi_version_cache(pkg_name, version):
     TMP_CACHE[pkg_name] = version
 
 
-FAKE_MANIFEST = """
-customer_name: ACME Inc.
-odoo_version: '16.0'
-customer_shortname: acme
-repo_name: acme_odoo
-project_id: '1234'
-project_name: acme_odoo
-odoo_company_name: ACME Inc.
-country: ch
-odoo_main_lang: de_DE
-odoo_aux_langs: fr_CH;it_IT
-platform_name: azure
-"""
+FAKE_MANIFEST_DATA = dict(
+    customer_name="ACME Inc.",
+    odoo_version="14.0",
+    customer_shortname="acme",
+    repo_name="acme_odoo",
+    project_id="1234",
+    project_name="acme_odoo",
+    odoo_company_name="ACME Inc.",
+    country="ch",
+    odoo_main_lang="de_DE",
+    odoo_aux_langs="fr_CH;it_IT",
+    platform_name="azure",
+)
 
 
-def make_fake_project_root(marker_file=get_root_marker(), req_file="requirements.txt"):
+def make_fake_project_root(
+    manifest=None, marker_file=get_root_marker(), req_file="requirements.txt"
+):
+    data = FAKE_MANIFEST_DATA.copy()
+    data.update(manifest or {})
+    # create empty file
     with open(marker_file, "w") as fd:
-        fd.write(FAKE_MANIFEST)
+        fd.write("")
+    # write YAML
+    update_yml_file(marker_file, data)
     with open(req_file, "w") as fd:
         fd.write("")
 
 
 @contextmanager
-def fake_project_root(make_root=True):
+def fake_project_root(make_root=True, manifest=None):
     runner = CliRunner()
     # TODO: do we really need this click util
     # or tmpfile api is enough?
     with runner.isolated_filesystem():
         if make_root:
-            make_fake_project_root()
+            make_fake_project_root(manifest=manifest)
         yield runner
--- tests/test_utils_proj.py
+++ tests/test_utils_proj.py
@@ -4,10 +4,31 @@
 from odoo_tools.utils import proj as proj_utils
 
 from .common import fake_project_root
+from .fixtures import clear_caches  # noqa
 
 
-def test_get_project_manifest():
+def test_get_project_manifest_1():
     with fake_project_root():
+        manifest = proj_utils.get_project_manifest()
+        expected = {
+            'country': 'ch',
+            'customer_name': 'ACME Inc.',
+            'customer_shortname': 'acme',
+            'odoo_aux_langs': 'fr_CH;it_IT',
+            'odoo_company_name': 'ACME Inc.',
+            'odoo_main_lang': 'de_DE',
+            'odoo_version': '14.0',
+            'platform_name': 'azure',
+            'project_id': '1234',
+            'project_name': 'acme_odoo',
+            'repo_name': 'acme_odoo',
+        }
+        for k, v in expected.items():
+            assert manifest[k] == v
+
+
+def test_get_project_manifest_2():
+    with fake_project_root(manifest=dict(odoo_version="16.0", project_id="4321")):
         manifest = proj_utils.get_project_manifest()
         expected = {
             'country': 'ch',
@@ -18,7 +39,7 @@ def test_get_project_manifest():
             'odoo_main_lang': 'de_DE',
             'odoo_version': '16.0',
             'platform_name': 'azure',
-            'project_id': '1234',
+            'project_id': '4321',
             'project_name': 'acme_odoo',
             'repo_name': 'acme_odoo',
         }
