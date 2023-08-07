409620af0114eaf363820c8ca0b52eef974dd6bf
Simone Orsi <simahawk@gmail.com>
2023-08-07 14:59:02 +0200

    cli.pending.show_prs: allow to filter and purge

--- odoo_tools/cli/pending.py
+++ odoo_tools/cli/pending.py
@@ -13,9 +13,16 @@ def cli():
 
 @cli.command(name="show")
 @click.argument("repo_path")
-def show_pending(repo_path):
+@click.option("-s", "--state", "state")
+@click.option(
+    "-p",
+    "--purge",
+    "purge",
+    type=click.Choice(['closed', 'merged'], case_sensitive=False),
+)
+def show_pending(repo_path, state=None, purge=None):
     repo = pm_utils.Repo(repo_path)
-    repo.show_prs()
+    repo.show_prs(state=state, purge=purge)
 
 
 if __name__ == "__main__":
