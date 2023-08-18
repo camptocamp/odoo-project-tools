12e7369fe7bed119eeedc460652d3f5d1af1abdc
Simone Orsi <simahawk@gmail.com>
2023-08-18 17:02:04 +0200

    cli.add_pending: aggregate by default

--- tests/test_addon_add_pending.py
+++ tests/test_addon_add_pending.py
@@ -1,13 +1,14 @@
 # Copyright 2023 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
-import os
-
 from odoo_tools.cli import addon
 from odoo_tools.utils.pending_merge import Repo
 from odoo_tools.utils.pkg import Package
+from odoo_tools.utils.req import get_project_dev_req
 
 from .common import fake_project_root
 from .fixtures import clear_caches  # noqa
+
+# TODO: test w/ aggregate
 
 
 def test_add_new_pending_no_addons():
@@ -18,12 +19,9 @@ def test_add_new_pending_no_addons():
         assert not repo.has_pending_merges()
         result = runner.invoke(
             addon.add_pending,
-            [
-                pr,
-            ],
+            [pr, "--no-aggregate"],
         )
         expected = [
-            "Adding: https://github.com/OCA/edi-framework/pull/1",
             "No addon specifified. Please update dev requirements manually.",
         ]
         assert result.output.splitlines() == expected
@@ -36,18 +34,19 @@ def test_add_new_pending_with_addon_editable():
     addon_name = "edi_oca"
     with fake_project_root(manifest=dict(odoo_version="16.0")) as runner:
         repo = Repo(repo_name, path_check=False)
-        pkg = Package(addon_name)
+        req_path = get_project_dev_req()
+        assert not req_path.exists()
+        pkg = Package(addon_name, req_filepath=req_path)
         assert not repo.has_pending_merges()
-        assert not pkg.is_editable()
-        assert not pkg.is_local()
-        assert not os.path.exists(os.getcwd() + "/dev_requirements.txt")
-        result = runner.invoke(addon.add_pending, [pr, "-a", addon_name])
+        result = runner.invoke(
+            addon.add_pending, [pr, "-a", addon_name, "--no-aggregate"]
+        )
         expected = [
-            "Adding: https://github.com/OCA/edi-framework/pull/1",
+            f"Adding: {addon_name} from https://github.com/OCA/edi-framework/pull/1",
             f"Updated dev requirements for: {addon_name}",
         ]
         assert result.output.splitlines() == expected
-        assert os.path.exists(os.getcwd() + "/dev_requirements.txt")
+        assert req_path.exists()
         assert repo.has_pending_merges()
         assert pkg.is_editable()
         assert pkg.is_local()
@@ -59,21 +58,19 @@ def test_add_new_pending_with_addon_not_editable():
     addon_name = "edi_oca"
     with fake_project_root(manifest=dict(odoo_version="16.0")) as runner:
         repo = Repo(repo_name, path_check=False)
-        pkg = Package(addon_name)
+        req_path = get_project_dev_req()
+        assert not req_path.exists()
+        pkg = Package(addon_name, req_filepath=req_path)
         assert not repo.has_pending_merges()
-        assert not pkg.has_pending_merge()
-        assert not pkg.is_editable()
-        assert not pkg.is_local()
-        assert not os.path.exists(os.getcwd() + "/dev_requirements.txt")
         result = runner.invoke(
-            addon.add_pending, [pr, "-a", addon_name, "--no-editable"]
+            addon.add_pending, [pr, "-a", addon_name, "--no-editable", "--no-aggregate"]
         )
         expected = [
-            "Adding: https://github.com/OCA/edi-framework/pull/1",
+            f"Adding: {addon_name} from https://github.com/OCA/edi-framework/pull/1",
             f"Updated dev requirements for: {addon_name}",
         ]
         assert result.output.splitlines() == expected
-        assert os.path.exists(os.getcwd() + "/dev_requirements.txt")
+        assert req_path.exists()
         assert repo.has_pending_merges()
         assert not pkg.is_editable()
         assert not pkg.is_local()
