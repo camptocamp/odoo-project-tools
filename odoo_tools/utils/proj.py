13bd5b973f8be803d3c3eb04d2466090d973ccf3
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2024-01-30 18:11:16 +0100

    support otools-project local-odoo --env .env
    allow setting up a local virtual environment with odoo and the project installed.

--- odoo_tools/utils/proj.py
+++ odoo_tools/utils/proj.py
@@ -1,8 +1,14 @@
 # Copyright 2023 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+import os
+import shutil
+import subprocess
+import venv
 from functools import lru_cache
 
 from ..config import get_conf_key
+from . import ui
+from .misc import get_template_path
 from .path import build_path, get_root_marker, root_path
 from .yaml import yaml_load
 
@@ -25,3 +31,70 @@ def get_current_version(serie_only=False):
     if serie_only:
         ver = ver.split(".")[0]
     return ver
+
+
+def setup_venv(venv_dir, odoo_src_path=None):
+    venv_dir = build_path(venv_dir)
+    ensure_local_requirements(build_path("local-requirements.txt"))
+    if (venv_dir / "pyvenv.cfg").is_file():
+        ui.echo(f"Reusing existing venv {venv_dir}")
+    else:
+        venv.create(venv_dir, with_pip=True)
+    pip = venv_dir / "bin/pip"
+    if odoo_src_path is None:
+        odoo_src_path = build_path(get_conf_key("odoo_src_rel_path"))
+
+    if not (venv_dir / "bin/odoo").is_file():
+        subprocess.run([pip, "install", "-r", odoo_src_path / "requirements.txt"])
+        subprocess.run([pip, "install", "-r", "local-requirements.txt"])
+    subprocess.run([pip, "install", "-r", build_path("requirements.txt")])
+    if build_path("dev_requirements.txt").is_file():
+        subprocess.run([pip, "install", "-r", build_path("dev_requirements.txt")])
+    subprocess.run([pip, "install", "-e", "."])
+
+
+def ensure_local_requirements(local_requirement_path):
+    local_requirement_tmpl = get_template_path("local-requirements.txt")
+    if not local_requirement_path.is_file():
+        shutil.copy(local_requirement_tmpl, local_requirement_path)
+    # TODO handle locally modified local-requirements.txt
+
+
+def generate_odoo_config_file(
+    venv_dir,
+    odoo_src_path,
+    odoo_enterprise_path,
+    config_file="odoo.cfg",
+    database_name=None,
+):
+    if database_name is None:
+        database_name = os.path.dirname(root_path())
+    config_file = build_path(config_file)
+    if config_file.is_file():
+        ui.echo(f"Reusing existing configuration file {config_file}")
+    else:
+        odoo = build_path(venv_dir) / "bin/odoo"
+        addons_dir = build_path("odoo/addons")
+
+        subprocess.call(
+            [
+                odoo,
+                "--save",
+                "-c",
+                config_file,
+                "-d",
+                database_name,
+                f"--addons-path={addons_dir}, {odoo_enterprise_path},{odoo_src_path}/addons,{odoo_src_path}/odoo/addons",
+                "--workers=0",
+                "--stop-after-init",
+            ]
+        )
+    config_has_running_env = False
+    with open(config_file) as odoo_cfg:
+        for line in odoo_cfg:
+            if line.strip().startswith("running_env"):
+                config_has_running_env = True
+                break
+    if not config_has_running_env:
+        with open(config_file, "a+") as odoo_cfg:
+            odoo_cfg.write("\nrunning_env=dev\n")
