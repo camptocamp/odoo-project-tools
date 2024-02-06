cab9b0a10b778afb5fdf1eb08b4558c0b1b3d2e8
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2024-02-06 21:50:45 +0100

    add tests

--- tests/test_project.py
+++ tests/test_project.py
@@ -4,9 +4,16 @@
 import os
 from unittest import mock
 
-from odoo_tools.cli.project import init
+from odoo_tools.cli.project import init, local_odoo
+from odoo_tools.config import get_conf_key
+from odoo_tools.utils.path import build_path
 
-from .common import compare_line_by_line, fake_project_root, get_fixture
+from .common import (
+    compare_line_by_line,
+    fake_project_root,
+    get_fixture,
+    mock_subprocess_run,
+)
 from .fixtures import clear_caches  # noqa
 
 
@@ -91,3 +98,168 @@ def test_init_custom_version():
             expected = get_fixture("expected.bumpversion.v2.cfg")
             compare_line_by_line(content, expected)
         assert result.exit_code == 0
+
+
+def test_local_odoo():
+    with fake_project_root(proj_tmpl_ver="2", proj_version="16.0.1.1.0") as runner:
+        odoo_src_path = str(build_path(get_conf_key("odoo_src_rel_path")))
+        odoo_enterprise_path = str(os.path.join(odoo_src_path, "..", "enterprise"))
+        mock_fn = mock_subprocess_run(
+            [
+                {
+                    "args": [
+                        "git",
+                        "clone",
+                        "--quiet",
+                        "--branch",
+                        "16.0",
+                        "git@github.com:odoo/odoo",
+                        odoo_src_path,
+                    ],
+                    # "sim_call": sim_touch,
+                    # "sim_call_args": ["foo"],
+                },
+                {
+                    "args": [
+                        "git",
+                        "-C",
+                        odoo_src_path,
+                        "checkout",
+                        "12345",
+                    ],
+                    # "sim_call": sim_touch,
+                    # "sim_call_args": ["foo"],
+                },
+                {
+                    "args": [
+                        "git",
+                        "clone",
+                        "--quiet",
+                        "--branch",
+                        "16.0",
+                        "git@github.com:odoo/enterprise",
+                        odoo_enterprise_path,
+                    ],
+                    # "sim_call": sim_touch,
+                    # "sim_call_args": ["foo"],
+                },
+                {
+                    "args": [
+                        "git",
+                        "-C",
+                        odoo_enterprise_path,
+                        "checkout",
+                        "56789",
+                    ],
+                    # "sim_call": sim_touch,
+                    # "sim_call_args": ["foo"],
+                },
+            ]
+        )
+        with mock.patch("subprocess.run", mock_fn):
+            runner.invoke(
+                local_odoo,
+                ["--odoo-hash", "12345", "--enterprise-hash", "56789"],
+                catch_exceptions=False,
+            )
+
+
+def test_local_odoo_venv():
+    with fake_project_root(proj_tmpl_ver="2", proj_version="16.0.1.1.0") as runner:
+        odoo_src_path = str(build_path(get_conf_key("odoo_src_rel_path")))
+        odoo_enterprise_path = str(os.path.join(odoo_src_path, "..", "enterprise"))
+        config_file = build_path("odoo.cfg")
+
+        def create_config():
+            with open(config_file, "w") as fobj:
+                fobj.write('db_name=testdb\n')
+
+        mock_fn = mock_subprocess_run(
+            [
+                {
+                    "args": [
+                        "git",
+                        "clone",
+                        "--quiet",
+                        "--branch",
+                        "16.0",
+                        "git@github.com:odoo/odoo",
+                        odoo_src_path,
+                    ],
+                    # "sim_call": sim_touch,
+                    # "sim_call_args": ["foo"],
+                },
+                {
+                    "args": [
+                        "git",
+                        "-C",
+                        odoo_src_path,
+                        "checkout",
+                        "12345",
+                    ],
+                    # "sim_call": sim_touch,
+                    # "sim_call_args": ["foo"],
+                },
+                {
+                    "args": [
+                        "git",
+                        "clone",
+                        "--quiet",
+                        "--branch",
+                        "16.0",
+                        "git@github.com:odoo/enterprise",
+                        odoo_enterprise_path,
+                    ],
+                    # "sim_call": sim_touch,
+                    # "sim_call_args": ["foo"],
+                },
+                {
+                    "args": [
+                        "git",
+                        "-C",
+                        odoo_enterprise_path,
+                        "checkout",
+                        "56789",
+                    ],
+                    # "sim_call": sim_touch,
+                    # "sim_call_args": ["foo"],
+                },
+                {
+                    "args": lambda a: str(a[0]).endswith('python')
+                    and a[2] == "ensurepip",
+                },
+                {
+                    "args": lambda a: str(a[0]).endswith('pip')
+                    and str(a[3]).endswith("odoo/src/requirements.txt"),
+                },
+                {
+                    "args": lambda a: str(a[0]).endswith('pip')
+                    and a[1:] == ["install", "-r", "local-requirements.txt"]
+                },
+                {
+                    "args": lambda a: str(a[0]).endswith('pip')
+                    and str(a[3]).endswith("/requirements.txt")
+                },
+                {
+                    "args": lambda a: str(a[0]).endswith('pip')
+                    and a[1:] == ['install', '-e', '.']
+                },
+                {
+                    "args": None,
+                    "sim_call": create_config,
+                },  # odoo config file generation, tested elsewhere
+            ]
+        )
+        with mock.patch("subprocess.run", mock_fn):
+            runner.invoke(
+                local_odoo,
+                [
+                    "--odoo-hash",
+                    "12345",
+                    "--enterprise-hash",
+                    "56789",
+                    '--venv',
+                    '.venv',
+                ],
+                catch_exceptions=False,
+            )
--- tests/test_utils_proj.py
+++ tests/test_utils_proj.py
@@ -1,9 +1,13 @@
 # Copyright 2023 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo_tools.utils import proj as proj_utils
+from unittest import mock
 
-from .common import fake_project_root
+from odoo_tools.config import get_conf_key
+from odoo_tools.utils import proj as proj_utils
+from odoo_tools.utils.path import build_path
+
+from .common import compare_line_by_line, fake_project_root, mock_subprocess_run
 from .fixtures import clear_caches  # noqa
 
 
@@ -51,3 +55,44 @@ def test_get_project_manifest_key():
     with fake_project_root():
         assert proj_utils.get_project_manifest_key("project_id") == "1234"
         assert proj_utils.get_project_manifest_key("customer_shortname") == "acme"
+
+
+def test_generate_odoo_config_file():
+    with fake_project_root(proj_tmpl_ver="2", proj_version="16.0.1.1.0"):
+        odoo_src_path = build_path(get_conf_key("odoo_src_rel_path"))
+        odoo_enterprise_path = str(odoo_src_path / "../enterprise")
+        venv_dir = build_path(".venv")
+        odoo_exec = venv_dir / "bin/odoo"
+        addons_dir = build_path("odoo/addons")
+        config_file = build_path("odoo.cfg")
+
+        def create_config():
+            with open(config_file, "w") as fobj:
+                fobj.write('db_name=testdb\n')
+
+        mock_fn = mock_subprocess_run(
+            [
+                {
+                    "args": [
+                        odoo_exec,
+                        "--save",
+                        "-c",
+                        config_file,
+                        "-d",
+                        "testdb",
+                        f"--addons-path={addons_dir}, {odoo_enterprise_path},{odoo_src_path}/addons,{odoo_src_path}/odoo/addons",
+                        "--workers=0",
+                        "--stop-after-init",
+                    ],
+                    "sim_call": create_config,
+                },
+            ]
+        )
+        with mock.patch("subprocess.run", mock_fn):
+            proj_utils.generate_odoo_config_file(
+                venv_dir, odoo_src_path, odoo_enterprise_path, database_name="testdb"
+            )
+        with open(config_file) as fobj:
+            config = fobj.read()
+        expected = "db_name=testdb\n\nrunning_env=dev\n"
+        compare_line_by_line(config, expected)
