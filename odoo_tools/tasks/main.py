823736d2eca6a01e34b24066c55775cf1b98b787
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2024-09-25 10:41:44 +0200

    Reintroduce the submodule task

--- odoo_tools/tasks/main.py
+++ odoo_tools/tasks/main.py
@@ -1,6 +1,12 @@
 from invoke import Collection, Program
 
-from . import database, lastpass, module, translate
+from . import (
+    database,
+    lastpass,
+    module,
+    # submodule,
+    translate,
+)
 
 tasks_ns = Collection()
 
@@ -8,5 +14,5 @@ tasks_ns.add_collection(database)
 tasks_ns.add_collection(lastpass)
 tasks_ns.add_collection(module)
 tasks_ns.add_collection(translate)
-
+# tasks_ns.add_collection(submodule)
 program = Program(namespace=tasks_ns, version="0.1.0")
