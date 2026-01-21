a5cbd5aa72782ed0912361a3438063d748d702a1
Iván Todorovich <ivan.todorovich@gmail.com>
2026-01-21 15:13:40 -0300

    [REM] tasks/submodule: unused file
    This was detected thanks to the type checker: ty.

--- odoo_tools/tasks/main.py
+++ odoo_tools/tasks/main.py
@@ -6,8 +6,6 @@ from . import (
 )
 
 tasks_ns = Collection()
-
 tasks_ns.add_collection(Collection.from_module(lastpass))
 tasks_ns.add_collection(Collection.from_module(module))
-# tasks_ns.add_collection(submodule)
 program = Program(namespace=tasks_ns, version="0.1.0")
