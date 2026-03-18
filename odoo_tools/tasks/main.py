32adb1f6ad1d9cddf0a3f8b648bea4b8893bf4ac
Iván Todorovich <ivan.todorovich@gmail.com>
2026-03-18 10:28:44 -0300

    [REM] otools-tasks: nothing left here

--- odoo_tools/tasks/main.py
+++
@@ -1,9 +0,0 @@
-from invoke import Collection, Program
-
-from . import (
-    module,
-)
-
-tasks_ns = Collection()
-tasks_ns.add_collection(Collection.from_module(module))
-program = Program(namespace=tasks_ns, version="0.1.0")
